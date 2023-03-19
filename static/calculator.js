ea641142162193082786d24c6459789bbbbb84a3
Logan Kragt <ljkrag@tgw-group.com>
2023-03-18 21:11:56 -0400

    This is a new commit for what I originally planned to be amended

---
+++ static/calculator.js
@@ -0,0 +1,32 @@
+let current_number = "";
+let calc_array = [];
+
+function updateDisplay(number) {
+    current_number += number;
+    document.getElementById("output").innerHTML = current_number;
+}
+
+function clearData() {
+    calc_array = [];
+    document.getElementById("output").innerHTML = "0";
+}
+function appendToArray(operator) {
+    calc_array.push(parseFloat(current_number));
+    calc_array.push(operator);
+    current_number = "";
+}
+
+function sendCalculation() {
+    calc_array.push(parseFloat(current_number));
+    $.ajax({
+        url: '/calculate',
+        type: 'POST',
+        data: JSON.stringify(calc_array),
+        contentType: 'application/json',
+        success: function(response) {
+            document.getElementById("output").innerHTML = response;
+            //calc_array = [];
+            //current_number = "";
+        }
+    });
+}
